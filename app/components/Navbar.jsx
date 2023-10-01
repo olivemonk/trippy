33c75b20c49a13a940e6de8623399f9f90d6de5f
OliveMonk <gp7.official@gmail.com>
2023-10-02 00:52:21 +0530

    added nav dropdown

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,27 @@
 "use client";
+import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
 
 import {
   Navbar,
+  DropdownTrigger,
+  DropdownItem,
   NavbarBrand,
   NavbarContent,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
   NavbarItem,
   Link,
+  DropdownMenu,
+  NavbarMenu,
+  Dropdown,
+  NavbarMenuItem,
+  NavbarMenuToggle,
   Button,
 } from "@nextui-org/react";
 import Image from "next/image";
-// import { AcmeLogo } from "./AcmeLogo.jsx";
+import { Lock, Activity, Flash, Server, TagUser, Scale} from "./Icons.jsx";
 
 export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuItems = [
     "Profile",
     "Dashboard",
@@ -28,28 +35,105 @@ export default function App() {
     "Log Out",
   ];
 
+  const icons = {
+    chevron: <FaChevronDown fill="currentColor" size={16} />,
+    scale: <Scale className="text-warning" fill="currentColor" size={30} />,
+    lock: <Lock className="text-success" fill="currentColor" size={30} />,
+    activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
+    flash: <Flash className="text-primary" fill="currentColor" size={30} />,
+    server: <Server className="text-success" fill="currentColor" size={30} />,
+    user: <TagUser className="text-danger" fill="currentColor" size={30} />,
+  };
+
   return (
     <Navbar>
-      <NavbarContent className="sm:hidden pr-3" justify="start">
-        <NavbarBrand>
-          {/* <AcmeLogo /> */}
-          <Image src="./logo.svg" width={38} height={38} alt="logo" />
-        </NavbarBrand>
-      </NavbarContent>
-
+      <NavbarBrand>
+        <Image
+          src="./logo.svg"
+          className="flex sm:hidden"
+          width={38}
+          height={38}
+          alt="logo"
+        />
+        <Image
+          src="./logo.svg"
+          className="hidden sm:flex"
+          width={28}
+          height={28}
+          alt="logo"
+        />
+        <p className=" hidden sm:flex font-bold text-inherit text-blue-600 text-xl">
+          Trippy
+        </p>
+      </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-          {/* <AcmeLogo /> */}
-          <Image src="./logo.svg" width={38} height={38} alt="logo" />
-          <p className="text-inherit text-blue-600 font-extrabold">Trippy</p>
-        </NavbarBrand>
         <NavbarItem>
           <Link color="foreground" href="#">
             Features
           </Link>
         </NavbarItem>
+
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                endContent={icons.chevron}
+                radius="sm"
+                variant="light"
+              >
+                Features
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            className="w-[340px]"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem
+              key="autoscaling"
+              description="ACME scales apps to meet user demand, automagically, based on load."
+              startContent={icons.scale}
+            >
+              Autoscaling
+            </DropdownItem>
+            <DropdownItem
+              key="usage_metrics"
+              description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+              startContent={icons.activity}
+            >
+              Usage Metrics
+            </DropdownItem>
+            <DropdownItem
+              key="production_ready"
+              description="ACME runs on ACME, join us and others serving requests at web scale."
+              startContent={icons.flash}
+            >
+              Production Ready
+            </DropdownItem>
+            <DropdownItem
+              key="99_uptime"
+              description="Applications stay on the grid with high availability and high uptime guarantees."
+              startContent={icons.server}
+            >
+              +99% Uptime
+            </DropdownItem>
+            <DropdownItem
+              key="supreme_support"
+              description="Overcome any challenge with a supporting team ready to respond."
+              startContent={icons.user}
+            >
+              +Supreme Support
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
         <NavbarItem isActive>
-          <Link href="#" aria-current="page" color="warning">
+          <Link href="#" aria-current="page">
             Customers
           </Link>
         </NavbarItem>
@@ -59,34 +143,35 @@ export default function App() {
           </Link>
         </NavbarItem>
       </NavbarContent>
-
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Button as={Link} color="primary" size="sm" href="#" variant="ghost">
+            Login
+          </Button>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="primary" size="sm" href="#" variant="solid">
             Sign Up
           </Button>
         </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden" justify="end">
-        <NavbarMenuToggle />
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
       </NavbarContent>
 
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
-              className="w-full"
               color={
                 index === 2
-                  ? "warning"
+                  ? "primary"
                   : index === menuItems.length - 1
                   ? "danger"
                   : "foreground"
               }
+              className="w-full"
               href="#"
               size="lg"
             >
